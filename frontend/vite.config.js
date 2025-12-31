22f43f035c2f9be62f319b86d36e8c81002a0f9f
bycrn <ninahmani@gmail.com>
2026-01-01 04:28:20 +0900

    supp readme

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -13,27 +13,5 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  **base: '/intro-to-csi-final-project/',**  // ← AJOUTE ÇA (nom de ton repo)
-  **build: {**
-    **outDir: '../dist'**  // ← Sortie à la racine du projet
-  **}**
+  base: '/intro-to-csi-final-project/'
 })
-
-// import { fileURLToPath, URL } from 'node:url'
-
-// import { defineConfig } from 'vite'
-// import vue from '@vitejs/plugin-vue'
-// import vueDevTools from 'vite-plugin-vue-devtools'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [
-//     vue(),
-//     vueDevTools(),
-//   ],
-//   resolve: {
-//     alias: {
-//       '@': fileURLToPath(new URL('./src', import.meta.url))
-//     },
-//   },
-// })
